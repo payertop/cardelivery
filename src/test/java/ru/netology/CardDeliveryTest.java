38c9b15f83dc469edb37c7785c22d266133deb65
payertop <skillsod55@gmail.com>
2023-03-27 23:11:37 +0700

    commit

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -1,91 +1,335 @@
 package ru.netology;
 
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
+
+
+
 
 public class CardDeliveryTest {
 
     @BeforeEach
-    public void openPage() {
-        open("http://localhost:9999/");
+    void openBrowser() {
+        open("http://localhost:9999");
     }
 
-
-    @Test
-    public void shouldValidValue() {
-        Configuration.timeout = 15000;
-        $("[placeholder='Город']").setValue("Москва");
-        $(".menu-item__control").click();
-//        $(".calendar-input__native-control").setValue("18.03.2023");
-        $("[name='name']").setValue("Олег Иванов");
-        $("[name='phone']").setValue("+79424242477");
-        $(".checkbox__box").click();
-        $(withText("Забронировать")).click();
-        $(withText("Успешно!")).shouldBe(visible);
-    }
-
-    private void selectPlusSevenDaysDate() {
-        LocalDate today = LocalDate.now();
-
-        LocalDate deliveryDate = LocalDate.now().plusDays(7);
-
-        if (deliveryDate.getMonthValue() != today.getMonthValue()) {
-            $("[type='button']").click();
-            $("[data-step='1']").click();
-        }
-
-        if (deliveryDate.getMonthValue() == today.getMonthValue()) {
-            $("[type='button']").click();
-        }
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
 
     @Test
-    public void shouldSuccessfullyWithManualFilling() {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.mm.yyyy");
-        String meetingDate = LocalDate.now().plusDays(3).format(formatter);
-        $("[placeholder='Город']").setValue("Москва");
-        $("[placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-        $("[placeholder='Дата встречи']").setValue(meetingDate);
-        $("[name='name']").setValue("Олег Иванов");
-        $("[name='phone']").setValue("+79424242477");
-        $(".checkbox__box").click();
-        $(withText("Забронировать")).click();
-        $(byText("Успешно!")).shouldBe(Condition.visible, Duration.ofMillis(15000));
-        $("[data-test-id='notification'] .notification__content")
-                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + meetingDate));
+    void shouldSendFormSuccessfulNotification() {
+        String planningDate = generateDate(3);
+        $x("//*[@data-test-id='city']//input").val("Тула");
+        $x("//*[@data-test-id='date']//input").doubleClick().sendKeys("BackSpace");
+        $x("//*[@data-test-id='date']//input").val(planningDate);
+        $x("//*[@data-test-id='name']//input").val("Джеймс Бонд");
+        $x("//*[@data-test-id='phone']//input").val("+79110070707");
+        $x("//*[@data-test-id='agreement']").click();
+        $x("//*[text()='Забронировать']").click();
+        $(".notification__content").shouldHave(Condition.text("Встреча успешно забронирована на "
+                + planningDate), Duration.ofSeconds(15)).shouldBe(visible);
     }
 
     @Test
-    public void shouldSuccessfullyWithAutocomplete() {
+    void shouldSendForm() {
+        String planningDate = generateDate(3);
+        $x("//*[@data-test-id='city']//input").val("Тула");
+        $x("//*[@data-test-id='date']//input").doubleClick().sendKeys("BackSpace");
+        $x("//*[@data-test-id='date']//input").val(planningDate);
+        $x("//*[@data-test-id='name']//input").val("Джеймс Бонд");
+        $x("//*[@data-test-id='phone']//input").val("+79110070707");
+        $x("//*[@data-test-id='agreement']").click();
+        $x("//*[text()='Забронировать']").click();
+        $x("//*[@data-test-id='notification']").shouldBe(visible, Duration.ofSeconds(14));
+    }
 
-        String meetingDateDay = String.valueOf(LocalDate.now().plusDays(7).getDayOfMonth());
+    @Test
+    void shouldSendFormCityWithDash() {
+        String planningDate = generateDate(3);
+        $x("//*[@data-test-id='city']//input").val("Санкт-Петербург");
+        $x("//*[@data-test-id='date']//input").doubleClick().sendKeys("BackSpace");
+        $x("//*[@data-test-id='date']//input").val(planningDate);
+        $x("//*[@data-test-id='name']//input").val("Джеймс Бонд");
+        $x("//*[@data-test-id='phone']//input").val("+79110070707");
+        $x("//*[@data-test-id='agreement']").click();
+        $x("//*[text()='Забронировать']").click();
+        $x("//*[@data-test-id='notification']").shouldBe(visible, Duration.ofSeconds(14));
+    }
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.mm.yyyy");
-        String meetingDate = LocalDate.now().plusDays(7).format(formatter);
+    //тест буквы "ё"
+    @Test
+    void shouldSendFormCityWithE() {
+        String planningDate = generateDate(3);
+        $x("//*[@data-test-id='city']//input").val("Орёл");
+        $x("//*[@data-test-id='date']//input").doubleClick().sendKeys("BackSpace");
+        $x("//*[@data-test-id='date']//input").val(planningDate);
+        $x("//*[@data-test-id='name']//input").val("Джеймс Бонд");
+        $x("//*[@data-test-id='phone']//input").val("+79110070707");
+        $x("//*[@data-test-id='agreement']").click();
+        $x("//*[text()='Забронировать']").click();
+        $x("//*[@data-test-id='notification']").shouldBe(visible, Duration.ofSeconds(14));
+    }
 
-        $("[placeholder='Город']").setValue("Мо");
-        $(byText("Москва")).click();
-        selectPlusSevenDaysDate();
-        $$(".calendar__day").find(text(meetingDateDay)).click();
-        $("[name='name']").setValue("Олег Иванов");
-        $("[name='phone']").setValue("+79424242477");
-        $(".checkbox__box").click();
-        $(withText("Забронировать")).click();
-        $(byText("Успешно!")).shouldBe(Condition.visible, Duration.ofMillis(15000));
-        $("[data-test-id='notification'] .notification__content")
-                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + meetingDate));
+    @Test
+    void shouldSendFormCityWithSpace() {
+        String planningDate = generateDate(3);
+        $x("//*[@data-test-id='city']//input").val("Нижний Новгород");
+        $x("//*[@data-test-id='date']//input").doubleClick().sendKeys("BackSpace");
+        $x("//*[@data-test-id='date']//input").val(planningDate);
+        $x("//*[@data-test-id='name']//input").val("Джеймс Бонд");
+        $x("//*[@data-test-id='phone']//input").val("+79110070707");
+        $x("//*[@data-test-id='agreement']").click();
+        $x("//*[text()='Забронировать']").click();
+        $x("//*[@data-test-id='notification']").shouldBe(visible, Duration.ofSeconds(14));
+    }
+
+    @Test
+    void shouldSendFormNameWithDash() {
+        String planningDate = generateDate(3);
+        $x("//*[@data-test-id='city']//input").val("Тула");
+        $x("//*[@data-test-id='date']//input").doubleClick().sendKeys("BackSpace");
+        $x("//*[@data-test-id='date']//input").val(planningDate);
+        $x("//*[@data-test-id='name']//input").val("Ана-Мария Огбезян");
+        $x("//*[@data-test-id='phone']//input").val("+79110070707");
+        $x("//*[@data-test-id='agreement']").click();
+        $x("//*[text()='Забронировать']").click();
+        $x("//*[@data-test-id='notification']").shouldBe(visible, Duration.ofSeconds(14));
+    }
+
+    @Test
+    void shouldSendFormSurNameWithDash() {
+        String planningDate = generateDate(3);
+        $x("//*[@data-test-id='city']//input").val("Тула");
+        $x("//*[@data-test-id='date']//input").doubleClick().sendKeys("BackSpace");
+        $x("//*[@data-test-id='date']//input").val(planningDate);
+        $x("//*[@data-test-id='name']//input").val("Марина Перейра-Родригес");
+        $x("//*[@data-test-id='phone']//input").val("+79110070707");
+        $x("//*[@data-test-id='agreement']").click();
+        $x("//*[text()='Забронировать']").click();
+        $x("//*[@data-test-id='notification']").shouldBe(visible, Duration.ofSeconds(14));
+    }
+
+    @Test
+    void shouldSendFormDate3Days() {
+        String planningDate = generateDate(3);
+        $x("//*[@data-test-id='city']//input").val("Тула");
+        $x("//*[@data-test-id='date']//input").doubleClick().sendKeys("BackSpace");
+        $x("//*[@data-test-id='date']//input").val(planningDate);
+        $x("//*[@data-test-id='name']//input").val("Марина Перейра-Родригес");
+        $x("//*[@data-test-id='phone']//input").val("+79110070707");
+        $x("//*[@data-test-id='agreement']").click();
+        $x("//*[text()='Забронировать']").click();
+        $x("//*[@data-test-id='notification']").shouldBe(visible, Duration.ofSeconds(14));
+    }
+
+    @Test
+    void shouldSendFormDate4Days() {
+        String planningDate = generateDate(4);
+        $x("//*[@data-test-id='city']//input").val("Тула");
+        $x("//*[@data-test-id='date']//input").doubleClick().sendKeys("BackSpace");
+        $x("//*[@data-test-id='date']//input").val(planningDate);
+        $x("//*[@data-test-id='name']//input").val("Марина Перейра-Родригес");
+        $x("//*[@data-test-id='phone']//input").val("+79110070707");
+        $x("//*[@data-test-id='agreement']").click();
+        $x("//*[text()='Забронировать']").click();
+        $x("//*[@data-test-id='notification']").shouldBe(visible, Duration.ofSeconds(14));
+    }
+
+    @Test
+    void shouldNotSendFormCityWrongSpelling() {
+        String planningDate = generateDate(3);
+        $x("//*[@data-test-id='city']//input").val("Санкт Петербург");
+        $x("//*[@data-test-id='date']//input").doubleClick().sendKeys("BackSpace");
+        $x("//*[@data-test-id='date']//input").val(planningDate);
+        $x("//*[@data-test-id='name']//input").val("Джеймс Бонд");
+        $x("//*[@data-test-id='phone']//input").val("+79110070707");
+        $x("//*[@data-test-id='agreement']").click();
+        $x("//*[text()='Забронировать']").click();
+        $x("//*[@data-test-id='city']//*[@class='input__sub']").shouldHave(exactText("Доставка в "
+                + "выбранный город недоступна"));
+    }
+
+    @Test
+    void shouldNotSendFormCityNotInList() {
+        String planningDate = generateDate(3);
+        $x("//*[@data-test-id='city']//input").val("Белозерск");
+        $x("//*[@data-test-id='date']//input").doubleClick().sendKeys("BackSpace");
+        $x("//*[@data-test-id='date']//input").val(planningDate);
+        $x("//*[@data-test-id='name']//input").val("Джеймс Бонд");
+        $x("//*[@data-test-id='phone']//input").val("+79110070707");
+        $x("//*[@data-test-id='agreement']").click();
+        $x("//*[text()='Забронировать']").click();
+        $x("//*[@data-test-id='city']//*[@class='input__sub']").shouldHave(exactText("Доставка в "
+                + "выбранный город недоступна"));
+    }
+
+    @Test
+    void shouldNotSendFormDateLessThanThreeDays() {
+        String planningDate = generateDate(2);
+        $x("//*[@data-test-id='city']//input").val("Вологда");
+        $x("//*[@data-test-id='date']//input").doubleClick().sendKeys("BackSpace");
+        $x("//*[@data-test-id='date']//input").val(planningDate);
+        $x("//*[@data-test-id='name']//input").val("Джеймс Бонд");
+        $x("//*[@data-test-id='phone']//input").val("+79110070707");
+        $x("//*[@data-test-id='agreement']").click();
+        $x("//*[text()='Забронировать']").click();
+        $x("//*[@data-test-id='date']//*[@class='input__sub']").shouldHave(exactText("Заказ на "
+                + "выбранную дату невозможен"));
+    }
+
+    @Test
+    void shouldNotSendFormDateInPast() {
+        String planningDate = generateDate(-365);
+        $x("//*[@data-test-id='city']//input").val("Вологда");
+        $x("//*[@data-test-id='date']//input").doubleClick().sendKeys("BackSpace");
+        $x("//*[@data-test-id='date']//input").val(planningDate);
+        $x("//*[@data-test-id='name']//input").val("Джеймс Бонд");
+        $x("//*[@data-test-id='phone']//input").val("+79110070707");
+        $x("//*[@data-test-id='agreement']").click();
+        $x("//*[text()='Забронировать']").click();
+        $x("//*[@data-test-id='date']//*[@class='input__sub']").shouldHave(exactText("Заказ на "
+                + "выбранную дату невозможен"));
+    }
+
+    @Test
+    void shouldNotSendFormNameInLatin() {
+        String planningDate = generateDate(3);
+        $x("//*[@data-test-id='city']//input").val("Вологда");
+        $x("//*[@data-test-id='date']//input").doubleClick().sendKeys("BackSpace");
+        $x("//*[@data-test-id='date']//input").val(planningDate);
+        $x("//*[@data-test-id='name']//input").val("James Bond");
+        $x("//*[@data-test-id='phone']//input").val("+79110070707");
+        $x("//*[@data-test-id='agreement']").click();
+        $x("//*[text()='Забронировать']").click();
+        $x("//*[@data-test-id='name']//*[@class='input__sub']").shouldHave(exactText("Имя и Фамилия "
+                + "указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void shouldNotSendFormSymbolsInsteadOfName() {
+        String planningDate = generateDate(3);
+        $x("//*[@data-test-id='city']//input").val("Вологда");
+        $x("//*[@data-test-id='date']//input").doubleClick().sendKeys("BackSpace");
+        $x("//*[@data-test-id='date']//input").val(planningDate);
+        $x("//*[@data-test-id='name']//input").val("~!@#$%^&*()_+?...;");
+        $x("//*[@data-test-id='phone']//input").val("+79110070707");
+        $x("//*[@data-test-id='agreement']").click();
+        $x("//*[text()='Забронировать']").click();
+        $x("//*[@data-test-id='name']//*[@class='input__sub']").shouldHave(exactText("Имя и Фамилия "
+                + "указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+    @Test
+    void shouldNotSendFormPhoneStartsWithEight() {
+        String planningDate = generateDate(3);
+        $x("//*[@data-test-id='city']//input").val("Вологда");
+        $x("//*[@data-test-id='date']//input").doubleClick().sendKeys("BackSpace");
+        $x("//*[@data-test-id='date']//input").val(planningDate);
+        $x("//*[@data-test-id='name']//input").val("Джеймс Бонд");
+        $x("//*[@data-test-id='phone']//input").val("89110070707");
+        $x("//*[@data-test-id='agreement']").click();
+        $x("//*[text()='Забронировать']").click();
+        $x("//*[@data-test-id='phone']//*[@class='input__sub']").shouldHave(exactText("Телефон указан "
+                + "неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    void shouldNotSendFormPhoneWithSpaces() {
+        String planningDate = generateDate(3);
+        $x("//*[@data-test-id='city']//input").val("Вологда");
+        $x("//*[@data-test-id='date']//input").doubleClick().sendKeys("BackSpace");
+        $x("//*[@data-test-id='date']//input").val(planningDate);
+        $x("//*[@data-test-id='name']//input").val("Джеймс Бонд");
+        $x("//*[@data-test-id='phone']//input").val("+7 911 007 07 07");
+        $x("//*[@data-test-id='agreement']").click();
+        $x("//*[text()='Забронировать']").click();
+        $x("//*[@data-test-id='phone']//*[@class='input__sub']").shouldHave(exactText("Телефон указан "
+                + "неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
+
+    @Test
+    void shouldNotSendFormIfNoAgreement() {
+        String planningDate = generateDate(3);
+        $x("//*[@data-test-id='city']//input").val("Вологда");
+        $x("//*[@data-test-id='date']//input").doubleClick().sendKeys("BackSpace");
+        $x("//*[@data-test-id='date']//input").val(planningDate);
+        $x("//*[@data-test-id='name']//input").val("Джеймс Бонд");
+        $x("//*[@data-test-id='phone']//input").val("+79110070707");
+        $x("//*[text()='Забронировать']").click();
+        $x("//*[@data-test-id='agreement']//*[@class='checkbox__text']").shouldHave(exactText("Я "
+                + "соглашаюсь с условиями обработки и использования моих персональных данных"));
+    }
+
+    @Test
+    void shouldNotSendFormIfEmptyCity() {
+        String planningDate = generateDate(3);
+        $x("//*[@data-test-id='city']//input").val("");
+        $x("//*[@data-test-id='date']//input").doubleClick().sendKeys("BackSpace");
+        $x("//*[@data-test-id='date']//input").val(planningDate);
+        $x("//*[@data-test-id='name']//input").val("Джеймс Бонд");
+        $x("//*[@data-test-id='phone']//input").val("+79110070707");
+        $x("//*[@data-test-id='agreement']").click();
+        $x("//*[text()='Забронировать']").click();
+        $x("//*[@data-test-id='city']//*[@class='input__sub']").shouldHave(exactText("Поле обязательно "
+                + "для заполнения"));
+    }
+
+    @Test
+    void shouldNotSendFormIfEmptyDate() {
+        $x("//*[@data-test-id='city']//input").val("Вологда");
+        $x("//*[@data-test-id='date']//input").doubleClick().sendKeys("BackSpace");
+        $x("//*[@data-test-id='date']//input").val("");
+        $x("//*[@data-test-id='name']//input").val("Джеймс Бонд");
+        $x("//*[@data-test-id='phone']//input").val("+79110070707");
+        $x("//*[@data-test-id='agreement']").click();
+        $x("//*[text()='Забронировать']").click();
+        $x("//*[@data-test-id='date']//*[@class='input__sub']").shouldHave(exactText("Неверно "
+                + "введена дата"));
+    }
+
+    @Test
+    void shouldNotSendFormIfEmptyName() {
+        String planningDate = generateDate(3);
+        $x("//*[@data-test-id='city']//input").val("Вологда");
+        $x("//*[@data-test-id='date']//input").doubleClick().sendKeys("BackSpace");
+        $x("//*[@data-test-id='date']//input").val(planningDate);
+        $x("//*[@data-test-id='name']//input").val("");
+        $x("//*[@data-test-id='phone']//input").val("+79110070707");
+        $x("//*[@data-test-id='agreement']").click();
+        $x("//*[text()='Забронировать']").click();
+        $x("//*[@data-test-id='name']//*[@class='input__sub']").shouldHave(exactText("Поле обязательно "
+                + "для заполнения"));
+    }
+
+    @Test
+    void shouldNotSendFormIfEmptyPhone() {
+        String planningDate = generateDate(3);
+        $x("//*[@data-test-id='city']//input").val("Вологда");
+        $x("//*[@data-test-id='date']//input").doubleClick().sendKeys("BackSpace");
+        $x("//*[@data-test-id='date']//input").val(planningDate);
+        $x("//*[@data-test-id='name']//input").val("Джеймс Бонд");
+        $x("//*[@data-test-id='phone']//input").val("");
+        $x("//*[@data-test-id='agreement']").click();
+        $x("//*[text()='Забронировать']").click();
+        $x("//*[@data-test-id='phone']//*[@class='input__sub']").shouldHave(exactText("Поле обязательно "
+                + "для заполнения"));
+    }
+
+    @Test
+    void shouldNotSendFormIfWholeFormEmpty() {
+        $x("//*[@data-test-id='date']//input").doubleClick().sendKeys("BackSpace");
+        $x("//*[text()='Забронировать']").click();
+        $x("//*[@data-test-id='city']//*[@class='input__sub']").shouldHave(exactText("Поле обязательно "
+                + "для заполнения"));
     }
 }
